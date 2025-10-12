187d9102c0942a5f05ed03769f5f89af144763d2
Erik <54033728+ErikGXDev@users.noreply.github.com>
2025-10-12 20:41:06 +0200

    Use different patch method

---
+++ osu.Game.Rulesets.UMania/Patches/LegacyBypass.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using osu.Game.Beatmaps;
+using osu.Game.Beatmaps.Formats;
+using osu.Game.Skinning;
+
+namespace osu.Game.Rulesets.UMania.Patches;
+
+[HarmonyPatch(typeof(LegacyBeatmapEncoder), MethodType.Constructor, new[] { typeof(IBeatmap), typeof(ISkin) })]
+public class LegacyBypass
+{
+    public static void Prefix(IBeatmap beatmap, ISkin? skin)
+    {
+        if (beatmap.BeatmapInfo.Ruleset.ShortName == "umania")
+        {
+            beatmap.BeatmapInfo.Ruleset.OnlineID = 3;
+        }
+    }
+
+    public static void Postfix(IBeatmap beatmap, ISkin? skin)
+    {
+        if (beatmap.BeatmapInfo.Ruleset.ShortName == "umania")
+        {
+            beatmap.BeatmapInfo.Ruleset.OnlineID = -1;
+        }
+    }
+}
